515d9ca65d7bf305f6dec204d92421f803ecda72
costetdivad12 <david.costet@iebem.edu.mx>
2022-05-13 18:39:20 -0500

    agregando componente gir-export

---
+++ src/GitExpertApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const GitExpertApp = props => {
+  return (
+    <>
+    <h2>GitExpertApp</h2>
+    <hr/>
+    </>
+  )
+}
+
+GitExpertApp.propTypes = {}
+
+export default GitExpertApp
