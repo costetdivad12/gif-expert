0ba5da5c5b6d3c4c60e2c364b25f1c5d6729231b
costetdivad12 <david.costet@iebem.edu.mx>
2022-05-17 16:15:27 -0500

    agregando lista al useState

--- src/GitExpertApp.js
+++ src/GitExpertApp.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const GitExpertApp = props => {
+
+  //  const categorias =['Sistemas','Pagos','Administracion'];
+  const [categorias, setCategorias] = useState(['Sistemas','Pagos','Administracion']);
+
+  const handleAdd=()=>{
+    //puede que se use de esta forma 
+    // setCategorias(['Juridico',...categorias]);
+    //o se puede utilizar de esta otra
+    setCategorias(categoria=> ['Juridico',...categoria]);
+  };
+
   return (
     <>
     <h2>GitExpertApp</h2>
     <hr/>
+    <button onClick={ handleAdd }>Agregar</button>
+     <ol>
+        {
+          categorias.map( ( categoria,i )=>{
+              return <li key={categoria}> {categoria}</li>
+          } )
+        }
+     </ol>
     </>
   )
 }
